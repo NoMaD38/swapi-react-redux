3954e42a16a3228d35893c9dcaca5d8275aa6230
NoMaD38 <nomad38note@gmail.com>
2020-08-31 21:24:43 +0300

    eslint

--- src/pages/Starships/index.jsx
+++ src/pages/Starships/index.jsx
@@ -41,6 +41,7 @@ export default function Starhips() {
             })
         }
         
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     },[ starships, valueSelect])
 
     useEffect(()=>{
